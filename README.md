3da4b7745564fb209e96aa58da7f28e9ba04a8da
Eugene Siow <sboonlin@dso.org.sg>
2019-10-16 13:14:11 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,23 @@
+# DSOViz Toolkit
+
+A fast, modern, reactive visualisation toolkit. The DSOViz Toolkit puts together a comprehensive suite of visualisation and visual analytics components.
+
+## Install the dependencies
+```bash
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+### Lint the files
+```bash
+npm run lint
+```
+
+### Build the app for production
+```bash
+quasar build
+```
---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: '/charts/', redirect: '/charts/radar' },
+      { path: '/charts/radar', name: 'Radar', component: () => import('pages/charts/Radar.vue') },
+      { path: '/charts/polar', name: 'Polar', component: () => import('pages/charts/Polar.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
