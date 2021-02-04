45b7049b7929e228ac4856b072882035b054094f
jiawei <jiaweilim97@gmail.com>
2021-02-04 12:02:40 +0800

    Dialog for adding new documents with field validations.

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,10 @@ module.exports = function (ctx) {
       directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Notify'],
+      config: {
+        notify: { /* look at QUASARCONFOPTIONS from the API card (bottom of page) */ }
+      }
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
--- src/components/datalabelling/documents/ImportDocument.vue
+++ src/components/datalabelling/documents/ImportDocument.vue
@@ -16,10 +16,29 @@
             v-model="documentToSubmit.name"
             label="Document Name"
             placeholder="E.g: All about Singapore"
-            :rules="[ val => val && val.length > 0 || 'Please input document name.']"
+            :rules="[ val => val && val.length > 0 || 'Please input document name.',
+            val => val.length <= 50 || 'Maximum name length is 50 characters.']"
           />
         </q-card-section>
         <q-card-section>
+            <q-file
+            name="upload_document"
+            ref="uploaddocument"
+            clearable multiple append dark filled use-chips bottom-slots
+            v-model="files"
+            label="Imports"
+            max-files="3" counter :counter-label="counterLabelFn"
+            :filter="checkFileSize"
+            accept=".txt"
+            lazy-rules :rules="[ val => val && val.length > 0 || 'Please select at least 1 document.']"
+             @rejected="onRejected">
+            <template v-slot:prepend>
+                <q-icon name="create_new_folder" @click.prevent/>
+            </template>
+            <template v-slot:hint>
+                 Maximum of 3 files per upload. File size not exceeding xxx.
+                </template>
+            </q-file>
         </q-card-section>
         <q-card-actions align="right" class="text-primary">
           <q-btn flat type="submit" label="Import" @click="submitNewDocument"/>
@@ -37,7 +56,7 @@ export default {
   props: ['dialog'],
   data () {
     return {
-      file: null,
+      files: null,
       model: null,
       secondDialog: false,
       documentToSubmit: {
@@ -59,12 +78,27 @@ export default {
   },
   methods: {
     ...mapActions('labels', ['addLabel']),
+    counterLabelFn ({ totalSize, filesNumber, maxFiles }) {
+      return `${filesNumber} files of ${maxFiles} | ${totalSize}`
+    },
+    checkFileSize (files) {
+      return files.filter(file => file.size < 2048)
+    },
+    onRejected (rejectedEntries) {
+      // https://quasar.dev/quasar-plugins/notify#Installation
+      console.log(rejectedEntries)
+      this.$q.notify({
+        type: 'negative',
+        message: `${rejectedEntries.length} file(s) did not pass validation constraints`
+      })
+    },
     submitNewDocument () {
       this.$refs.name.validate()
-      this.$refs.color.validate()
-      this.$refs.shortcutkey.validate()
-      if (!this.$refs.name.hasError && !this.$refs.color.hasError && !this.$refs.shortcutkey.hasError) {
-        this.submitLabel()
+      this.$refs.uploaddocument.validate()
+      if (!this.$refs.name.hasError && !this.$refs.uploaddocument.hasError) {
+        console.log('Submit')
+        console.log(this.files)
+        // this.submitLabel()
       }
     },
     submitLabel () {
@@ -78,11 +112,6 @@ export default {
     },
     close () {
       this.$emit('close')
-    },
-    availableShortkeys () {
-      const usedKeys = this.labels.map(item => item.shortcutkey)
-      const unusedKeys = this.shortcutkeys.filter(item => !usedKeys.includes(item))
-      return unusedKeys
     }
   }
 }
